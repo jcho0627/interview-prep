d8840f610633d480443c77a5bda4602e2d4596c2
June Hyok Cho <jcho0627@gmail.com>
2019-05-03 16:26:09 +0200

    build tower initial setup

---
+++ Algorithm/build-tower.js
@@ -0,0 +1,14 @@
+/*
+Build Tower by the following given argument:
+number of floors (integer and always greater than 0).
+
+Tower block is represented as *
+
+Test.assertDeepEquals(towerBuilder(1), ["*"]);
+Test.assertDeepEquals(towerBuilder(2), [" * ","***"]);
+Test.assertDeepEquals(towerBuilder(3), ["  *  "," *** ","*****"]);
+*/
+
+function towerBuilder(nFloors) {
+  // build here
+}
