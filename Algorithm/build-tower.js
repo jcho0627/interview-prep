6ee40b00ea200001ee90fcfb26232147a4c402a6
June Hyok Cho <jcho0627@gmail.com>
2019-05-08 17:18:08 +0200

    reviewed and added new refactored solution

--- Algorithm/build-tower.js
+++ Algorithm/build-tower.js
@@ -32,6 +32,17 @@ function towerBuilder(nFloors) {
   }
 
   return arr;
-}
+};
 
 console.log(towerBuilder(3));
+
+function refactorTower(nFloors) {
+  let tower = [];
+  for (let i = 0; i < nFloors; i++) {
+    tower.push(" ".repeat(nFloors - i - 1) + "*".repeat((i * 2) + 1) + " ".repeat(nFloors - i - 1));
+  }
+
+  return tower;
+};
+
+console.log(refactorTower(3));
