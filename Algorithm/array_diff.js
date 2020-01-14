cf5e028c301f898cddf178adbfc0ff72a59de012
June Hyok Cho <jcho0627@gmail.com>
2020-01-14 13:03:16 +0100

    array_diff first iteration and refactor

--- Algorithm/array_diff.js
+++ Algorithm/array_diff.js
@@ -13,14 +13,11 @@ array_diff([3,4], [3]), [4], "a was [3,4], b was [3]"
 array_diff([1,8,2], []), [1,8,2], "a was [1,8,2], b was []"
 */
 
+// First Successful Iteration
 function array_diff(a, b) {
-  if (a.length === 0) {
+  if (a.length === 0 || b.length === 0) {
     return a;
-  } else if (b.length === 0) {
-    return b;
   } else {
-    // a.sort();
-
     let result = a.filter(number => number !== b[0]);
 
     for (let i = 1; i < b.length; i++) {
@@ -31,9 +28,21 @@ function array_diff(a, b) {
   }
 }
 
+// Refactor of First Iteration
+function array_diff_refactor(a, b) {
+  return a.filter(function(x) { return b.indexOf(x) == -1 });
+}
+
 console.log(array_diff([], [4, 5]))
 console.log(array_diff([1, 8, 2], []))
 console.log(array_diff([4, 3], [3]))
 console.log(array_diff([1, 1, 4, 3, 4, 4], [4, 3]))
 console.log(array_diff([5, 6, 3, 2, 8, 9, 5, 6, 7, 1, 2, 1], [5, 6, 7]))
 console.log(array_diff([5, 6, 3, 2, 8, 9, 5, 6, 7, 1, 2, 1], [1, 2]))
+
+console.log(array_diff_refactor([], [4, 5]))
+console.log(array_diff_refactor([1, 8, 2], []))
+console.log(array_diff_refactor([4, 3], [3]))
+console.log(array_diff_refactor([1, 1, 4, 3, 4, 4], [4, 3]))
+console.log(array_diff_refactor([5, 6, 3, 2, 8, 9, 5, 6, 7, 1, 2, 1], [5, 6, 7]))
+console.log(array_diff_refactor([5, 6, 3, 2, 8, 9, 5, 6, 7, 1, 2, 1], [1, 2]))
